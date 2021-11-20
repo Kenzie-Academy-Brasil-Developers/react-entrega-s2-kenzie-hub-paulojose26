a4c8268e09b943c9ca766bbea4dd4162a3b7e516
PauloJose26 <paulojoliveira.2616@gmail.com>
2021-11-20 16:08:52 -0300

    feat: adicionando o Formulario no cadastro

--- src/pages/Cadastre/index.jsx
+++ src/pages/Cadastre/index.jsx
@@ -1,8 +1,9 @@
-
+import { Section } from "./style";
 
 import Form from "../../components/Form";
-import Button from "../../components/Button";
 import Input from "../../components/Input";
+import Radio from "../../components/Radio";
+import Button from "../../components/Button";
 
 const Cadastre = () => {
     return (
@@ -12,6 +13,15 @@ const Cadastre = () => {
             <Input id="contact" type="text" placeholder="Contato" />
             <Input id="bio" type="text" placeholder="Bio" />
             <label htmlFor="">Selecionar módulo:</label>
+            <Section>
+                <Radio id="first--module" name="module" placeholder="Primeiro" />
+                <Radio id="second--module" name="module" placeholder="Segundo" />
+                <Radio id="third--module" name="module" placeholder="Terceiro" />
+                <Radio id="fourth--module" name="module" placeholder="Quarto" />
+            </Section>
+            <Input id="password" type="text" placeholder="Senha"/>
+            <Input id="confirm--password" type="text" placeholder="Confirme a senha"/>
+            <Button type="submit">Cadastrar</Button>
         </Form>
     );
 };
---
+++ src/pages/Cadastre/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    width: 100%;
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
